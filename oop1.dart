106c95b9e7ac57bc2fc6e554cd1ad5555f16eb97
KingsMunene <munenekings2@gmail.com>
2024-03-21 20:21:47 +0300

    Added three classes

---
+++ oop1.dart
@@ -0,0 +1,47 @@
+class Roam{
+  void roaming(){
+    print("Roaming");
+  }
+}
+
+abstract class Animal extends Roam{
+  abstract String food;
+  abstract String habitat;
+  
+  void eat();
+  void makeNoise();
+}
+
+class  Dog extends Animal {
+  @override
+  String food;
+
+  @override
+  String habitat;
+
+
+
+  Dog(this.food, this.habitat);
+
+  @override
+  void eat() {
+    print("The Dog is eating $food");
+  }
+
+  @override
+  void makeNoise() {
+    print("woof Woof");
+  }
+  @override
+  void roaming() {
+    print("The dog is Roaming");
+  }
+  
+}
+
+void main(List<String> args) {
+  var dog = Dog("Meet", "Kennel");
+
+  dog.makeNoise();
+  dog.Roaming();
+}
